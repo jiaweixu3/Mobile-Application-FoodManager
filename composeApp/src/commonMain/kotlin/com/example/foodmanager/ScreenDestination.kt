99199ec308b160a52603e27140471f6b91859870
alvarodca <19acanizo@gmail.com>
2026-02-06 19:38:08 -0600

    #3 Added Navigation Bar at the bottom which goes to the different available screens

--- composeApp/src/commonMain/kotlin/com/example/foodmanager/ScreenDestination.kt
+++ composeApp/src/commonMain/kotlin/com/example/foodmanager/ScreenDestination.kt
@@ -4,12 +4,33 @@ package com.example.foodmanager
 // Uses Serializable as data type passing is safer and sealed to avoid compilation errors
 
 import kotlinx.serialization.Serializable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.ui.graphics.vector.ImageVector
 
+
+// Sealed allows for a fixed number of screen, avoids future bugs as well as it checks all elements
 sealed interface ScreenDestination {
+    // All screens must have a title and an image, for easier navigation
+    val title: String
+    val icon: ImageVector
+    @Serializable
+    data object Inventory: ScreenDestination{
+        override val title =  "Inventory"
+        override val icon =  Icons.Filled.List
+    }
 
     @Serializable
-    data object Inventory: ScreenDestination
+    data object ShoppingList: ScreenDestination{
+        override val title = "ShoppingList"
+        override val icon = Icons.Filled.ShoppingCart
+    }
+
     @Serializable
-    data object ShoppingList: ScreenDestination
-    @Serializable
-    data object AddItem : ScreenDestination}
+    data object AddItem : ScreenDestination{
+        override val title = "AddItem"
+        override val icon = Icons.Filled.Add
+    }
+}
