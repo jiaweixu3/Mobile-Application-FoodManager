b6bbcaa1294e4d5c539fba262c66b33db9af485c
alvarodca <19acanizo@gmail.com>
2026-02-24 19:47:49 -0500

    #19 Issue 2. Created basic settings screen, missing connection with rest of the app

---
+++ composeApp/src/commonMain/kotlin/com/example/foodmanager/ui/settings/SettingsScreen.kt
@@ -0,0 +1,42 @@
+package com.example.foodmanager.ui.settings
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.ui.graphics.Color
+
+
+@Composable
+fun SettingsScreen(logoutSuccess: () -> Unit) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(title = { Text("Settings") })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.fillMaxSize().padding(innerPadding),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            // Actual logout button
+            Button(
+                onClick = {
+                    logoutSuccess()
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Red)
+
+            ) {
+                Text(text = "Log Out")
+            }
+        }
+    }
+}
