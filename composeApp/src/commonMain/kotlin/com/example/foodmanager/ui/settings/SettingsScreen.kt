33b331a9b409a694f7f0ea5508aea1d43f06f02a
alvarodca <19acanizo@gmail.com>
2026-03-11 20:55:41 -0400

    #15. Added basic implementation to the settings screen, missing dropdown menu

--- composeApp/src/commonMain/kotlin/com/example/foodmanager/ui/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/foodmanager/ui/settings/SettingsScreen.kt
@@ -2,6 +2,7 @@ package com.example.foodmanager.ui.settings
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -13,23 +14,69 @@ import androidx.compose.ui.Modifier
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 // Defines simple settings screen, for now it only contains log out.
 @OptIn(ExperimentalMaterial3Api::class) // Needed for the Top App Bar to function correctly
 @Composable
-fun SettingsScreen(logoutSuccess: () -> Unit) {
+fun SettingsScreen(
+    logoutSuccess: () -> Unit, // Handles logging out logic
+    onHouseholdSelected: (String) -> Unit = {} // Handles current household
+) {
+    // Available households, for now this is mock data
+    val availableHouseholds = listOf("house 1", "house 2", "house 3")
+
+
+    // Controls if dropdown menu is active or not
+    var expandedDropdown by remember {mutableStateOf(false)}
+
+    // Stores current households, first is active by default
+    var currentHousehold by remember { mutableStateOf(availableHouseholds[0]) }
+
     Scaffold(
         topBar = {
             CenterAlignedTopAppBar(title = { Text("Settings") })
         }
     ) { innerPadding ->
+        // General column for the settings screen
         Column(
             modifier = Modifier.fillMaxSize().padding(innerPadding),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            // Actual logout button
+            // Household Management
+            Text(
+                text = "Households",
+                style = MaterialTheme.typography.titleLarge,
+                modifier = Modifier
+                    .padding(bottom = 16.dp)
+
+            )
+            ExposedDropdownMenuBox(
+               expanded = expandedDropdown,
+                onExpandedChange = { expandedDropdown = it },
+            ){
+                OutlinedTextField(
+                    value = currentHousehold,
+                    onValueChange = {},
+                    readOnly = true,
+                    label = {Text("Current Household")}
+                )
+            }
+            // Separation
+            Spacer(modifier = Modifier.padding(8.dp))
+
+
+
+            // Logout
             Button(
                 onClick = {
                     logoutSuccess()
@@ -39,6 +86,10 @@ fun SettingsScreen(logoutSuccess: () -> Unit) {
             ) {
                 Text(text = "Log Out")
             }
+
+
+
+
         }
     }
 }
